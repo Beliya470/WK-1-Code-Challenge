cbb21565012b4f399eacf4a2c7ad01ce6a4fabf5
Beliya470 <anneanziay78@gmail.com>
2023-06-19 09:54:31 +0300

    more amendmends

--- speedDetector.js
+++ speedDetector.js
@@ -1,18 +1,24 @@
-function checkSpeed(speed) {
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
   const speedLimit = 70;
   const kmPerPoint = 5;
-
-  if (speed < speedLimit) {
-      console.log("Ok");
-  } else {
-      let points = Math.floor((speed - speedLimit) / kmPerPoint);
-      if (points >= 12) {
-          console.log("License suspended");
-      } else {
-          console.log("Points:", points);
-      }
-  }
-}
-
-// Test the function
-checkSpeed(80);
+  
+  readline.question('Enter speed: ', speed => {
+    speed = parseInt(speed);
+  
+    if (speed < speedLimit) {
+        console.log("Ok");
+    } else {
+        let points = Math.floor((speed - speedLimit) / kmPerPoint);
+        if (points >= 12) {
+            console.log("License suspended");
+        } else {
+            console.log("Points:", points);
+        }
+    }
+  
+    readline.close();
+  });  
--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -1,24 +1,24 @@
 const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-readline.question('Enter student marks (0-100): ', marks => {
-  marks = parseInt(marks);
-
-  if (marks < 0 || marks > 100) {
-      console.log("Invalid marks. Please enter a number between 0 and 100.");
-  } else if (marks >= 80) {
-      console.log("Grade: A");
-  } else if (marks >= 60) {
-      console.log("Grade: B");
-  } else if (marks >= 50) {
-      console.log("Grade: C");
-  } else if (marks >= 40) {
-      console.log("Grade: D");
-  } else {
-      console.log("Grade: E");
-  }
-
-  readline.close();
-});
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Enter student marks (0-100): ', marks => {
+    marks = parseInt(marks);
+  
+    if (marks < 0 || marks > 100) {
+        console.log("Invalid marks. Please enter a number between 0 and 100.");
+    } else if (marks >= 80) {
+        console.log("Grade: A");
+    } else if (marks >= 60) {
+        console.log("Grade: B");
+    } else if (marks >= 50) {
+        console.log("Grade: C");
+    } else if (marks >= 40) {
+        console.log("Grade: D");
+    } else {
+        console.log("Grade: E");
+    }
+  
+    readline.close();
+  });  
